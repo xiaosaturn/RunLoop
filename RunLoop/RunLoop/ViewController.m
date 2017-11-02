e2daad4122d367b1d30eded34163f2628d992df7
Xiao <807132689@qq.com>
2017-11-02 22:59:16 +0800

    o

--- RunLoop/RunLoop/ViewController.m
+++ RunLoop/RunLoop/ViewController.m
@@ -10,19 +10,30 @@
 
 @interface ViewController ()
 
+@property (nonatomic,strong) dispatch_source_t timer;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    //来一个队列
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    //创建一个定时器
+    self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    
+    //设置定时器
+    dispatch_time_t start = DISPATCH_TIME_NOW;
+    dispatch_time_t interval = 1.0 * NSEC_PER_SEC;
+    dispatch_source_set_timer(self.timer, start, interval, 0);
+    dispatch_source_set_event_handler(self.timer, ^{
+        NSLog(@"----%@----%@",[NSRunLoop currentRunLoop],[NSThread currentThread]);
+    });
+    //启动定时器
+    dispatch_resume(self.timer);
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
