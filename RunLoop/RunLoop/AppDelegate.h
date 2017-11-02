e01adf5f64ef1f0879d3ee0057687a5191060db2
Xiao <807132689@qq.com>
2017-11-02 18:32:31 +0800

    初始化

---
+++ RunLoop/RunLoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunLoop
+//
+//  Created by Xiao on 2017/11/2.
+//  Copyright © 2017年 com.xiao.forward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunLoop/RunLoop/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RunLoop
+//
+//  Created by Xiao on 2017/11/2.
+//  Copyright © 2017年 com.xiao.forward. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
